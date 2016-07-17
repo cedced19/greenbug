83b49c4998c16c5ffaa89e85eeaea49dab821edd
Cédric <cedced19@gmail.com>
2016-07-17 10:42:02 +0200

    Add tests for 'bugs' and 'projects' collection and delete not used dependencies

--- test/collections.js
+++ test/collections.js
@@ -26,6 +26,8 @@ suite('Test models', function () {
     setup(function (done) {
         waterline.loadCollection(require('../models/users.js'));
         waterline.loadCollection(require('../models/registrants.js'));
+        waterline.loadCollection(require('../models/bugs.js'));
+        waterline.loadCollection(require('../models/projects.js'));
         waterline.initialize(config, function  (err, ontology) {
             if (err) {
                 return done(err);
@@ -79,5 +81,31 @@ suite('Test models', function () {
             });
     });
 
+    test('should be able to create a bug and link it to a project', function (done) {
+        var Projects = waterline.collections.projects;
+        var Bugs = waterline.collections.bugs;
 
+        Projects.create({
+            title: 'Pizzayolo',
+            description: 'A application to order a pizza.'
+        })
+        .then(function (project) {
+            assert.equal(project.title, 'Pizzayolo', 'should have set the title');
+            assert.equal(project.description, 'A application to order a pizza.', 'should have set the description');
+            assert.equal(project.bugs.length, 0, 'should have no tasks');
+            Bugs.create({
+                description: 'Cannot order a royal pizza.',
+                project: project.id
+            }).then(function(bug) {
+                assert.equal(bug.description, 'Cannot order a royal pizza.', 'should have set the description');
+                Bugs.find()
+                .populate('project')
+                .exec(function(err, bug) {
+                    if (err) throw err;
+                    assert.equal(bug[0].project.title, 'Pizzayolo', 'should have linked to a project');
+                    done();
+                });
+            });
+        });
+    });
 });
