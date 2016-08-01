5f02dc7bf40c444c8bfc6b4f0cf6ddbea53bfabf
cedced19 <cedced19@gmail.com>
2016-08-01 11:24:14 -0400

    Upgrade admin policy

--- policies/admin.js
+++ policies/admin.js
@@ -1,10 +1,13 @@
 // Test if user is authenticated and if user is an administrator
 module.exports = function(req, res, next) {
-   if (req.isAuthenticated() && req.user.admin) {
-        next();
+  if (req.isAuthenticated()) {
+    if (req.user.admin) {
+      return next();
+    }
+    var err = new Error('You don\'t have the required abilities.');
+    err.status = 401;
+    return next(err);
   } else {
-        var err = new Error('You don\'t have the required abilities.');
-        err.status = 401;
-        next(err);
+    res.redirect('/login');
   }
 };
